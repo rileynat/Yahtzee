d78c29fcb9979842a0450551bc22167ad5d004a0
rileynat <rileynat@umich.edu>
2013-12-02 11:10:08 -0500

    adding in formatting

--- Dice.java
+++ Dice.java
@@ -2,44 +2,50 @@ package eecs285.proj4.Yahtzee;
 
 import java.util.Random;
 
-public class Dice {
-	final private int NUMDICE = 5;
-	private int [] die_values;
-	private Random randomizer;
-	private boolean[] die_locked;
-	
-	public Dice(int seed) {
-		randomizer = new Random(seed);
-		die_locked = new boolean[NUMDICE];
-		die_values = new int[NUMDICE];
-		for(int i=0;i<NUMDICE;i++){
-			die_locked[i]=false;
-			die_values[i]=6;
-		}
-	}
-	
-	void roll(){
-		for(int i=0;i<NUMDICE;i++){
-			if(die_locked[i]==false){
-				die_values[i] = randomizer.nextInt(6)+1;
-			}
-		}
-	}
-	
-	void toggle_lock_die(final int die){
-		if(die>=0 && die<=NUMDICE-1){
-			die_locked[die] = !die_locked[die];
-		}
-	}
-	
-	final int[] get_dice_values(){
-		return die_values;
-	}
-	
-	boolean is_die_locked(final int die_index){
-		if(die_index>=0 && die_index<=NUMDICE-1){
-			return die_locked[die_index];
-		}
-		return true;
-	}
+public class Dice
+{
+   final private int NUMDICE = 5;
+   private int[] die_values;
+   private Random randomizer;
+   private boolean[] die_locked;
+
+   public Dice(int seed)
+   {
+      randomizer = new Random(seed);
+      die_locked = new boolean[NUMDICE];
+      die_values = new int[NUMDICE];
+      for (int i = 0; i < NUMDICE; i++) {
+         die_locked[i] = false;
+         die_values[i] = 6;
+      }
+   }
+
+   void roll()
+   {
+      for (int i = 0; i < NUMDICE; i++) {
+         if (die_locked[i] == false) {
+            die_values[i] = randomizer.nextInt(6) + 1;
+         }
+      }
+   }
+
+   void toggle_lock_die(final int die)
+   {
+      if (die >= 0 && die <= NUMDICE - 1) {
+         die_locked[die] = !die_locked[die];
+      }
+   }
+
+   final int[] get_dice_values()
+   {
+      return die_values;
+   }
+
+   boolean is_die_locked(final int die_index)
+   {
+      if (die_index >= 0 && die_index <= NUMDICE - 1) {
+         return die_locked[die_index];
+      }
+      return true;
+   }
 }
--- Yahtzee_main.java
+++ Yahtzee_main.java
@@ -5,78 +5,77 @@ import javax.swing.JFrame;
 import java.util.ArrayList;
 import java.util.Date;
 
+public class Yahtzee_main
+{
 
-
-public class Yahtzee_main {
-   
    public final static String IPADDRESS = "67.194.123.241";
    public final static String FXBIPADDRESS = "67.194.113.232";
    public static ArrayList<String> playerNames;
-   
-	public static void main(String [] args){
-	   boolean success = false;
-	   boolean serverInUse = true;
-	   String myPlayerName = "";
-	      String string = "";
-	      String recvdStr = "";
-	      int numPlayers = 4;
-	      ClientServerSocket client;
-	      playerNames = new ArrayList<String>();
-	      String [] players = new String[4];
-	      client = new ClientServerSocket(IPADDRESS, 45548);
-	      if (serverInUse) {
-	      client.startClient();
-	      }
-	      while (!success) {
-	         PlayerNamesDialog namesDialog = new PlayerNamesDialog(new JFrame(),
-	               "Player Names Input");
-	         myPlayerName = namesDialog.getName();
-	         if (myPlayerName.trim().length() > 0) {
-	            success = true;
-	         }
-	      }
-	      if (serverInUse) {
-	      client.sendString(myPlayerName);
-	      System.out.println("test before recieving string");
-	      recvdStr = client.recvString();
-	      while (recvdStr == "") {
-	         client.sendString(string);
-	         recvdStr = client.recvString();
-	         System.out.print(recvdStr);
-	      }
-	      
-	      numPlayers = client.recvInt();
-	      System.out.println("Received number of players from server: "
-	            + numPlayers);
-	      recvdStr = client.recvString();
-	      System.out.println("Received this message from server: " + recvdStr);
-	      for (int i = 0; i < numPlayers && i < 4; i++) {
-	         recvdStr = client.recvString();
-	         System.out.println("Received player name from server: " + recvdStr);
-	         players[i] = recvdStr;
-	      }
-	      
-	      recvdStr = client.recvString();
-	      System.out.println("Received this message from server: " + recvdStr);
-	      recvdStr = client.recvString();
-	      System.out.println("Received this message from server: " + recvdStr);
-	      }
-	      
-		
-	      Date cur_date = new Date();
-	      long timestamp = cur_date.getTime();
-	      int rand = (int) timestamp & 0xFFFFFFFF;
-		Yahtzee_GUI gui = new Yahtzee_GUI(numPlayers, rand, players, myPlayerName, client);
-		gui.pack();		
-		gui.setVisible(true);
-		gui.setDefaultCloseOperation(
-		JFrame.EXIT_ON_CLOSE);
-		if(players[0].equals(myPlayerName)){
-		   System.out.println(myPlayerName);
-			gui.start_turn();
-		}else{		
-			gui.get_Server_data();
-		}
-	}
+
+   public static void main(String[] args)
+   {
+      boolean success = false;
+      boolean serverInUse = true;
+      String myPlayerName = "";
+      String string = "";
+      String recvdStr = "";
+      int numPlayers = 4;
+      ClientServerSocket client;
+      playerNames = new ArrayList<String>();
+      String[] players = new String[4];
+      client = new ClientServerSocket(IPADDRESS, 45548);
+      if (serverInUse) {
+         client.startClient();
+      }
+      while (!success) {
+         PlayerNamesDialog namesDialog = new PlayerNamesDialog(new JFrame(),
+               "Player Names Input");
+         myPlayerName = namesDialog.getName();
+         if (myPlayerName.trim().length() > 0) {
+            success = true;
+         }
+      }
+      if (serverInUse) {
+         client.sendString(myPlayerName);
+         System.out.println("test before recieving string");
+         recvdStr = client.recvString();
+         while (recvdStr == "") {
+            client.sendString(string);
+            recvdStr = client.recvString();
+            System.out.print(recvdStr);
+         }
+
+         numPlayers = client.recvInt();
+         System.out.println("Received number of players from server: "
+               + numPlayers);
+         recvdStr = client.recvString();
+         System.out.println("Received this message from server: " + recvdStr);
+         for (int i = 0; i < numPlayers && i < 4; i++) {
+            recvdStr = client.recvString();
+            System.out.println("Received player name from server: " + recvdStr);
+            players[i] = recvdStr;
+         }
+
+         recvdStr = client.recvString();
+         System.out.println("Received this message from server: " + recvdStr);
+         recvdStr = client.recvString();
+         System.out.println("Received this message from server: " + recvdStr);
+      }
+
+      Date cur_date = new Date();
+      long timestamp = cur_date.getTime();
+      int rand = (int) timestamp & 0xFFFFFFFF;
+      Yahtzee_GUI gui = new Yahtzee_GUI(numPlayers, rand, players,
+            myPlayerName, client);
+      gui.pack();
+      gui.setVisible(true);
+      gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      if (players[0].equals(myPlayerName)) {
+         System.out.println(myPlayerName);
+         gui.start_turn();
+      } else {
+         gui.get_Server_data();
+      }
+   }
 }
 //
